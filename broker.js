63f1f5fb96d105bd8c0b8ce773d6ef062aca5ac8
pichetz <pichets@zupports.co>
2023-12-21 13:40:42 +0700

    change mqtt connection

--- broker.js
+++ broker.js
@@ -1,15 +1,25 @@
 const mqtt = require('mqtt')
 require('dotenv').config();
 
-const broker = mqtt.connect({
-    host: process.env.MQTT_SERVER,
-    port: process.env.MQTT_PORT,
-    username: process.env.MQTT_USER,
-    password: process.env.MQTT_PASSWORD
-});
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
-broker.on('connect', function () {
-    console.log("MQTT Connect");
-});
+const connectUrl = `mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`
+
+const broker = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: process.env.MQTT_USER,
+    password: process.env.MQTT_PASSWORD,
+    reconnectPeriod: 1000,
+})
+//   console.log(broker)
+broker.on('connect', () => {
+    console.log(new Date(), 'Connected')
+})
+
+// broker.on('connect', function () {
+//     console.log("MQTT Connect");
+// });
 
 module.exports = broker
