f431d199d147fe8fe8579d9d53da7bba0003c101
pichetz <pichets@zupports.co>
2023-12-21 13:40:54 +0700

    [+] subscribe

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-require('dotenv').config();
 const broker = require('./broker')
 
 // TODO:: call MT4 API or something
+const topic = process.argv[2] ?? 'main'
+broker.subscribe(topic, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('subscribe on', topic)
+    }
+});
 broker.on('message', function (topic, message) {
-    console.log(topic, message.toString());
+    console.log(new Date(), topic, message.toString());
 });
